11a83e47794b87f91e7b8eb532ce4bc1531144dc
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-18 18:06:14 -0400

    geocoding + pulling results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from "react";
 import {Row, Col} from 'antd';
 import 'antd/dist/antd.css';
 import SearchInput from './SearchInput.js';
@@ -6,14 +7,17 @@ import SearchResults from './SearchResults.js';
 import LocationsMap from './LocationsMap.js';
 
 function App() {
+
+  const [results, setResults] = useState(null);
+  console.log("TOP LEVEL", results)
   return (
     <div style={{height: "100%"}}>
       <Row style={{height: "50%"}}>
-        <Col span={12}><SearchInput /></Col>
-        <Col span={12}><SearchResults /></Col>
+        <Col span={12}><SearchInput setResults={setResults}/></Col>
+        <Col span={12}><LocationsMap /></Col>
       </Row>
       <Row style={{height: "50%"}}>
-        <Col span={24}><LocationsMap /></Col>
+        <Col span={24}><SearchResults /></Col>
       </Row>
     </div>
   );
--- src/SearchInput.js
+++ src/SearchInput.js
@@ -1,9 +1,67 @@
+import { Form, Input, Button } from "antd";
+const axios = require('axios');
+const GOOGLE_PLACES_API_KEY = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
+const GOOGLE_GEOCODING_API_KEY = process.env.REACT_APP_GOOGLE_GEOCODING_API_KEY;
+const RADIUS = 10;
+const getLocation = (input) => {
 
+};
 
-function SearchInput() {
+function SearchInput(props) {
+
+  const search = (values) => {
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        key: GOOGLE_GEOCODING_API_KEY,
+        address: values.location
+      }
+    })
+    .then( (res) => {
+      console.log("location", res);
+      if (res.data.status === "OK") {
+        return res.data.results[0].geometry.location;
+      }
+      else {
+        console.log("NOPE")
+      }
+    })
+    .then ( (coords) => {
+      axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", {
+        params: {
+          key: GOOGLE_PLACES_API_KEY,
+          location: coords.lat + "," + coords.lng,
+          radius: RADIUS,
+          keyword: "restaurant OR bar"
+        }
+      })
+      .then ( (res) => {
+        console.log("results", res);
+        props.setResults(res.data.results);
+      })
+      .catch((error) => console.log(error))
+    })
+    .catch((error) => console.log(error))
+  };
+
   return (
     <div>
       SearchInput
+      <Form
+        onFinish={search}
+      >
+        <Form.Item
+          label="Location"
+          name="location"
+        >
+          <Input span={8}/>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Search
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
