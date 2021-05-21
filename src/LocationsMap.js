9aab270c4deeb7215d0c009a60bb305f7bbc7728
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-05-21 17:19:43 -0400

    added starter map

--- src/LocationsMap.js
+++ src/LocationsMap.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
+function LocationsMap(props) {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
 
-function LocationsMap() {
   return (
-    <div>
-      LocationsMap
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
   );
 }
 
